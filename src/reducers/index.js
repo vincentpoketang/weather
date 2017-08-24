613673c3c5ec04078500348ad894c70791d83269
Vincent Tang <vincet3@uci.edu>
2017-08-23 23:02:24 -0700

    add action files and started working on reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import WeatherReducder from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducder
 });
 
 export default rootReducer;
